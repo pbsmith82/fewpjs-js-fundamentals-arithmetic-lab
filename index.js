0300aa91219867a92222d8bfda327b8b805dc046
pbsmith82 <pbsmith82@gmail.com>
2021-04-04 14:24:24 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+
+let num1 = 31
+let num2 = 2
+
+
+let multiply = num1 * num2
+let random = parseInt(Math.random() + 2)
+
+let num3 = 39
+let num4 = 5
+
+let mod = num3 % num4
+
+let max = Math.max(12, 15, 18, 20)
